4f5b904d0a997c470f2f8acb1094bc80b5abc254
Centur1es <93638341+Centur1es@users.noreply.github.com>
2022-01-11 21:30:00 +0800

    Delete Loop.java

--- Loop.java
+++
@@ -1,9 +0,0 @@
-public class Loop {
-    public static void main(String[] args) {
-        for(int i=102;i>66;i--){
-            if(i%2==1){
-                System.out.println("Odd Number :"+i);
-            }
-        }
-    }
-}
